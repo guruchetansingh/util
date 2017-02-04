9df7d24f4ca2e5bdd9b6823e943af15a30ecd61d
guruchetansingh <guruchetansingh@gmail.com>
2017-02-04 16:46:08 +0530

    has three digits after decimal
    Double object can be passed now

--- util/AmountValidation.java
+++ util/AmountValidation.java
@@ -1,11 +1,15 @@
 package com.createfakecontacts.util;
+/**
+ * Created by guru on 03-02-2017.
+ */
 
 import android.util.Log;
 
 import java.math.BigDecimal;
+import java.util.StringTokenizer;
 
 /**
- * Created by guruchetan on 18-01-2017.
+ * Created by guru
  */
 
 //        .0 , .00 , 0. , 0.0 , 0.00 , 00. , 00.0 , 00.00
@@ -21,6 +25,10 @@ import java.math.BigDecimal;
 //        Log.i("AmountValidation", "valid .99 = " + new AmountValidation().isValidAmount(".99"));
 //        Log.i("AmountValidation", "valid 9. = " + new AmountValidation().isValidAmount("9."));
 //        Log.i("AmountValidation", "valid 9,78 = " + new AmountValidation().isValidAmount("9,78"));
+//        Log.i("AmountValidation", "valid 9.782 = " + new AmountValidation().isValidAmount("9.782"));
+//        Log.i("AmountValidation", "valid 9..782 = " + new AmountValidation().isValidAmount("9..782"));
+//        Log.i("AmountValidation", "valid .. = " + new AmountValidation().isValidAmount(".."));
+//    Log.i("AmountValidation", "new Double(\"..\") = " + new AmountValidation().isValidAmount(new Double("..")));
 public class AmountValidation {
 
     private final String TAG = "AmountValidation";
@@ -28,18 +36,33 @@ public class AmountValidation {
     /**
      * Maximum amount range eg. 0-2000 or 10-10000 or 5-400 etc
      */
-    private final double MAX_RANG = 100000;
+    private double MAX_RANG;
 
     /**
      * Minimum amount range
      */
-    private final double MIN_RANG = 0;
+    private double MIN_RANG;
+
+    public AmountValidation() {
+    }
+
+    public AmountValidation(double MIN_RANG, double MAX_RANG) {
+        this.MIN_RANG = MIN_RANG;
+        this.MAX_RANG = MAX_RANG;
+
+    }
+
 
     /**/
     public boolean isValidAmount(String amount) {
 
+        if (hasThreeDigitAfterDecimal(amount))
+            return false;
+
+
         if (!isValidNumber(amount))
             return false;
+
 
         return true;
     }
@@ -65,6 +88,18 @@ public class AmountValidation {
             Log.e(TAG, "[Exception] = " + e.toString());
             return false;
         }
+    }
+
+    private boolean hasThreeDigitAfterDecimal(String number) {
+        StringTokenizer stringTokenizer = new StringTokenizer(number, ".");
+        if (stringTokenizer.countTokens() > 1)
+            return true;
+        while (stringTokenizer.hasMoreElements()) {
+            if (stringTokenizer.nextElement().toString().length() > 2)
+                return true;
+        }
+
+        return false;
     }
 
     /**
@@ -102,4 +137,14 @@ public class AmountValidation {
         return convertNumberToAmountFormat(amount + "");
     }
 
+    /**
+     * This method is used to get amount formatted string<br>
+     * eg. 1. to 1.00 or .0 to 0.00 or 12.123456 to 12.12
+     */
+    public String convertNumberToAmountFormat(Double amount) {
+        if (amount != null)
+            return convertNumberToAmountFormat(String.valueOf(amount));
+        else
+            return convertNumberToAmountFormat("");
+    }
 }
